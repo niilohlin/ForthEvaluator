081ed524637f44c1aade9923fc2c8049c9ca3911
Niil Öhlin <niilohlin@gmail.com>
2017-06-04 19:40:37 +0200

    can now define variables

--- ForthEvaluator.hs
+++ ForthEvaluator.hs
@@ -4,7 +4,7 @@ import qualified Data.Map as Map
 
 data NativeFunc = Drop | Plus | Apply | Define deriving (Show)
 
-data ProgramUnit = Word String | Native NativeFunc | ForthInt Int | ForthString String | ForthLambda [ProgramUnit] deriving (Show)
+data ProgramUnit = Word String | Symbol String | Native NativeFunc | ForthInt Int | ForthString String | ForthLambda [ProgramUnit] deriving (Show)
 
 type ProgramStack = [ProgramUnit]
 type Dict = (Map.Map String ProgramUnit)
@@ -17,8 +17,13 @@ parse = undefined
 evalNativeFunc :: NativeFunc -> ProgramState -> Either String ProgramState
 evalNativeFunc Drop (ProgramState {dataStack = []}) = Left "error, cannot drop from empty stack"
 evalNativeFunc Drop (p@(ProgramState {dataStack = (_:a)})) = Right $ p { dataStack = a }
-evalNativeFunc Plus p@(ProgramState { dataStack = ((ForthInt a):(ForthInt b):c)}) = Right $ p { dataStack = (ForthInt (a + b)):c}
+evalNativeFunc Plus p@(ProgramState { dataStack = ((ForthInt a):(ForthInt b):c)}) = Right $ p { dataStack = (ForthInt (a + b)):c }
 evalNativeFunc Plus a = Left ("error, cannot add " ++ show a)
+evalNativeFunc Define p@ProgramState {
+    dataStack = ((Symbol symbolName):definition:rest),
+    dict = oldDict
+    } = let newDict = Map.insert symbolName definition oldDict in Right $ p {dataStack = rest, dict = newDict}
+evalNativeFunc Define p = Left "cannot define"
 evalNativeFunc a stack = undefined
 
 eval :: ProgramState -> Either String ProgramStack
@@ -30,12 +35,12 @@ getDefinition dict key = case (Map.lookup key dict) of
                             Nothing -> Left $ "Word " ++ key ++ " not defined"
 
 evalOneStep :: ProgramState -> Either String ProgramState
-evalOneStep p@ProgramState{programStack = [] } = Right p
-evalOneStep p@ProgramState{programStack = ((Native n):prog)}  = evalNativeFunc n p
-evalOneStep p@ProgramState{programStack = ((Word word):prog) , dict = d } = getDefinition d word >>= \programUnit -> Right $ p { programStack = (programUnit:prog) }
-evalOneStep p@ProgramState{programStack = (a:prog), dataStack = d }= Right $ p { programStack = prog, dataStack = (a:d) }
+evalOneStep p@ProgramState { programStack = [] } = Right p
+evalOneStep p@ProgramState { programStack = ((Native n):prog)}  = evalNativeFunc n (p {programStack = prog})
+evalOneStep p@ProgramState { programStack = ((Word word):prog) , dict = d } = getDefinition d word >>= \programUnit -> Right $ p { programStack = (programUnit:prog) }
+evalOneStep p@ProgramState { programStack = (a:prog), dataStack = d }= Right $ p { programStack = prog, dataStack = (a:d) }
 
 
-exampleProgram =  [(Word "x"), (ForthInt 5), (ForthInt 4), Native Plus]
+exampleProgram =  [(ForthInt 5), (Symbol "y"), (Native Define), (Word "y")]
 exapmleProgramState = ProgramState exampleProgram [] (Map.fromList [("x", ForthInt 6 )])
 
